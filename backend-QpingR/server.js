41bb033ae96ecf3cf0e512f5a9de02595aef6d1b
sparsh-01M <113444357+sparsh-01M@users.noreply.github.com>
2025-03-17 21:17:05 +0530

    Update server.js

--- backend-QpingR/server.js
+++ backend-QpingR/server.js
@@ -10,7 +10,7 @@ const port = 5001;
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // Allow only your frontend
+    origin: "https://qping-r.vercel.app", // Allow only your frontend
     credentials: true, // Allow cookies, authorization headers
     methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
     allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
