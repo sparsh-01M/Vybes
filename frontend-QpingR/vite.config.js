651abed4b3b41702d3b745635aceef4164fd43d2
sparsh-01M <113444357+sparsh-01M@users.noreply.github.com>
2025-03-18 11:27:13 +0530

    Update vite.config.js

--- frontend-QpingR/vite.config.js
+++ frontend-QpingR/vite.config.js
@@ -1,7 +1,38 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000', // Replace with your API backend URL
+        changeOrigin: true,
+        secure: false,
+        configure: (proxy, options) => {
+          proxy.on('proxyReq', (proxyReq, req, res) => {
+            res.setHeader('Access-Control-Allow-Credentials', 'true')
+            res.setHeader('Access-Control-Allow-Origin', '*')
+            res.setHeader(
+              'Access-Control-Allow-Methods',
+              'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+            )
+            res.setHeader(
+              'Access-Control-Allow-Headers',
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+            )
+          })
+        }
+      }
+    }
+  }
 })
